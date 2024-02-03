a166358e9e0ca93d60c631900b521af96eff8300
Simon <simontorbett@gmail.com>
2024-02-03 15:06:28 -0300

    nuevo commit

--- src/paginas/anuncios.js
+++ src/paginas/anuncios.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../estilos/anuncios.css";
 import Logo from "../imagenes/img/logo.svg";
+import anuncio4 from "../imagenes/img/anuncio4.jpg";
+import anuncio5 from "../imagenes/img/anuncio5.jpg";
+import anuncio6 from "../imagenes/img/anuncio6.jpg";
+import wc from "../imagenes/img/icono_wc.svg";
+import estacionamiento from "../imagenes/img/icono_estacionamiento.svg";
+import cuartos from "../imagenes/img/icono_dormitorio.svg";
 
 export const Anuncios = () => {
 
@@ -24,7 +30,98 @@ export const Anuncios = () => {
         </div>
         </div>
         <div className="contenedor-seccion">
-            <h1>Anuncios</h1>
+                    <h2> Casas Y Departamentos en Venta</h2>
+
+                    <div className="contenedor-anuncios">
+                        <div className="anuncio">
+                            <picture>
+                                <img src={anuncio4} alt="anuncio"></img>
+                            </picture>
+                            <div className="contenido-anuncio">
+                                <h3> Casa de Lujo en el Lago</h3>
+                                <p>Casa en el Lago con excelente vista, acabados de lujo a un exclente precio</p>
+                                <p className="precio"> 90.000.000</p>
+                                <ul className="iconos-caracteristicas">
+                                    <li>
+                                        <img src={wc} alt="poceta"></img>
+                                        <p>3</p>
+
+                                    </li>
+                                    <li>
+                                        <img src={estacionamiento} alt="estacionamiento"></img>
+                                        <p>3</p>
+
+                                    </li>
+                                    <li>
+                                        <img src={cuartos} alt="cuartos"></img>
+                                        <p>4</p>
+
+                                    </li>
+
+                                </ul>
+                                <Link to="/anuncios" className="boton">Ver Propiedades</Link>
+                            </div>
+                        </div>
+                        <div className="anuncio">
+                            <picture>
+                                <img src={anuncio5} alt="anuncio"></img>
+                            </picture>
+                            <div className="contenido-anuncio">
+                                <h3> Casa Terminados de Lujo</h3>
+                                <p>Casa en el Lago con excelente vista, acabados de lujo a un exclente precio</p>
+                                <p className="precio"> 90.000.000</p>
+                                <ul className="iconos-caracteristicas">
+                                    <li>
+                                        <img src={wc} alt="poceta"></img>
+                                        <p>3</p>
+
+                                    </li>
+                                    <li>
+                                        <img src={estacionamiento} alt="estacionamiento"></img>
+                                        <p>3</p>
+
+                                    </li>
+                                    <li>
+                                        <img src={cuartos} alt="cuartos"></img>
+                                        <p>4</p>
+
+                                    </li>
+
+                                </ul>
+                                <Link to="/anuncios" className="boton">Ver Propiedades</Link>
+                            </div>
+                        </div>
+                        <div className="anuncio">
+                            <picture>
+                                <img src={anuncio6} alt="anuncio"></img>
+                            </picture>
+                            <div className="contenido-anuncio">
+                                <h3> Casa con piscina</h3>
+                                <p>Casa en el Lago con excelente vista, acabados de lujo a un exclente precio</p>
+                                <p className="precio"> 90.000.000</p>
+                                <ul className="iconos-caracteristicas">
+                                    <li>
+                                        <img src={wc} alt="poceta"></img>
+                                        <p>3</p>
+
+                                    </li>
+                                    <li>
+                                        <img src={estacionamiento} alt="estacionamiento"></img>
+                                        <p>3</p>
+
+                                    </li>
+                                    <li>
+                                        <img src={cuartos} alt="cuartos"></img>
+                                        <p>4</p>
+
+                                    </li>
+
+                                </ul>
+                                <Link to="/anuncios" className="boton">Ver Propiedades</Link>
+                            </div>
+                        </div>
+
+                    </div>
         </div>
         </body>
         </>
--- src/paginas/nosotros.js
+++ src/paginas/nosotros.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../estilos/nosotros.css";
 import Logo from "../imagenes/img/logo.svg";
+import nosotros from "../imagenes/img/nosotros.jpg";
+import icono1 from "../imagenes/img/icono1.svg";
+import icono2 from "../imagenes/img/icono2.svg";
+import icono3 from "../imagenes/img/icono3.svg";
 
 export const Nosotros = () => {
     return(
@@ -23,7 +27,50 @@ export const Nosotros = () => {
         </div>
         </div>
         <div className="contenedor-seccion">
-            <h1>Nosotros</h1>
+            <h1>Conoce sobre Nosotros</h1>
+            <div className="contenido-nosotros">
+
+                <picture>
+                    <img src={nosotros} alt="Sobre Nosotros" className="img"></img>
+                </picture>
+                <div className="texto-nosotros">
+                <blockquote>
+                    25 Años de Experiencia
+                </blockquote>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum laoreet volutpat. Sed enim sem, ultricies at venenatis eget, convallis a justo. Duis elementum auctor nunc, a feugiat eros cursus a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et erat vitae nibh venenatis rutrum feugiat vel dui. Donec elementum suscipit sapien in consequat. Etiam fermentum massa ut fermentum laoreet. Aliquam condimentum vestibulum nisi ut consequat. Sed sem augue, eleifend quis malesuada vel, euismod id ligula. Nulla pulvinar mi a eleifend egestas. Ut quis efficitur erat. Fusce convallis pretium nibh sed scelerisque. Duis ac maximus lorem. Integer sodales efficitur est, et consequat lorem ultrices a. Integer et luctus magna.
+
+Aenean gravida ante nec odio condimentum, a posuere orci viverra. Vivamus purus purus, tincidunt eu velit nec, aliquet auctor elit. Ut ultrices dictum augue vel tincidunt. Curabitur semper, nulla at aliquet maximus, ex elit cursus nulla, ac convallis augue arcu congue arcu. Quisque rutrum neque eu metus consequat, sit amet feugiat augue hendrerit. Pellentesque tincidunt, risus quis venenatis blandit, nunc dui suscipit ipsum, et pulvinar augue quam sit amet orci. Vestibulum lacus magna, efficitur nec finibus at, pulvinar vitae nibh. Vivamus urna est, suscipit vel varius non, pellentesque eu tellus.
+
+Donec tempor viverra purus nec volutpat. Integer et blandit diam. Nunc vel tempus augue, in elementum lacus. Pellentesque elementum metus non justo ultrices, nec accumsan eros posuere. Ut tortor tortor, posuere non nulla ac, laoreet vulputate lacus. Quisque pellentesque eros eget ligula pulvinar dictum. Phasellus vel auctor sem. Phasellus lobortis dictum nulla, in iaculis purus ullamcorper nec. Nunc a orci quam.
+                </p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar pretium est, non cursus felis facilisis at. Integer leo erat, sagittis ut purus a, hendrerit convallis mauris. Nulla nec justo mattis eros aliquam sodales. Nulla laoreet est dolor, ac gravida augue eleifend sollicitudin. Ut tincidunt feugiat arcu, at bibendum enim.
+                </p>
+            </div>
+
+            </div>
+            <div className="contenedor-seccion">
+                    <h1>Mas Sobre Nosotros</h1>
+                    <div className="icono-nosotros">
+                        <div className="icono">
+                            <img src={icono1} alt="candado"></img>
+                            <h3> Seguridad
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ornare orci. Suspendisse dictum lacus in blandit sodales. Mauris vitae nisi metus. Sed id euismod tortor, nec sollicitudin tellus. Proin eget efficitur mi. Nam dictum tincidunt vehicula. Suspendisse molestie quam magna, id pulvinar erat aliquam at. Ut suscipit dui ut nisl elementum mattis. </p>
+                            </h3></div>
+                        <div className="icono">
+                            <img src={icono2} alt="precio"></img>
+                            <h3>Precio
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ornare orci. Suspendisse dictum lacus in blandit sodales. Mauris vitae nisi metus. Sed id euismod tortor, nec sollicitudin tellus. Proin eget efficitur mi. Nam dictum tincidunt vehicula. Suspendisse molestie quam magna, id pulvinar erat aliquam at. Ut suscipit dui ut nisl elementum mattis. </p>
+                            </h3> </div>
+                        <div className="icono">
+                            <img src={icono3} alt="tiempo"></img>
+                            <h3>Tiempo
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ornare orci. Suspendisse dictum lacus in blandit sodales. Mauris vitae nisi metus. Sed id euismod tortor, nec sollicitudin tellus. Proin eget efficitur mi. Nam dictum tincidunt vehicula. Suspendisse molestie quam magna, id pulvinar erat aliquam at. Ut suscipit dui ut nisl elementum mattis. </p>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
         </div>
         </body>
         </>
