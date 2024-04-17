c9c3e9cf4520b3eaa19284300d58724a62a006be
Simon <simontorbett@gmail.com>
2024-04-17 19:34:34 -0400

    new commit

--- src/compoenentes/header.jsx
+++ src/compoenentes/header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import Logo from "../imagenes/img/logo.svg";
 import header from "../imagenes/img/header.jpg"
-import cellmenu from "../imagenes/img/barras.svg";
+// import cellmenu from "../imagenes/img/barras.svg";
 
 export const Header = ({ titulo, mostrarImagenDeFondo, alturaMinima }) => {
   const headerStyle = {
